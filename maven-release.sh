e54d51aff87959272c7a27abe09a8670b7869bf5
mozsi1krisza06 <mozsi.krisztian@ext.telekom.hu>
2020-09-10 23:43:26 +0200

    Add Dockerfile for build container

--- maven-release.sh
+++ maven-release.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+# Usage: ./maven-release.sh v2.0 2.1-SNAPSHOT 2.0
+
 branch_to_release="master"
+
+echo "Cloning convertapi-java..."
+git clone https://github.com/kmozsi/convertapi-java.git
+cd convertapi-java
 
 echo "Switching to the release branch..."
 git fetch
@@ -18,18 +25,7 @@ echo "Cleaning..."
 mvn clean
 
 echo "Preparing release..."
-if [ $# -eq 1 ]
-then
-	echo -en "\n\n" | mvn release:prepare -Dtag=$1
-elif [ $# -eq 2 ] 
-then
-	echo -en "\n" | mvn release:prepare -DdevelopmentVersion=$2 -Dtag=$1
-elif [ $# -eq 3 ] 
-then
-	mvn release:prepare -DreleaseVersion=$3 -DdevelopmentVersion=$2 -Dtag=$1
-else
-	echo -en "\n\n\n" | mvn release:prepare
-fi
+mvn release:prepare -DreleaseVersion=${VERSION} -DdevelopmentVersion=${NEXT_VERSION} -Dtag=v${VERSION} -Dresume=false
 
 echo "Performing release..."
 mvn release:perform

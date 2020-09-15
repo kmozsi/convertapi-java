79a23bfee4bbf844bb6c76175deb7f2e62d32576
Krisztián Mózsi <mozsik0@gmail.com>
2020-09-15 16:33:50 +0200

    fix docker release

--- maven-release.sh
+++ maven-release.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
 
-# Usage: ./maven-release.sh v2.0 2.1-SNAPSHOT 2.0
-
 branch_to_release="master"
 
 echo "Cloning convertapi-java..."
-git clone https://github.com/kmozsi/convertapi-java.git
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/kmozsi/convertapi-java.git
 cd convertapi-java
 
 echo "Switching to the release branch..."
 git fetch
 git checkout $branch_to_release
-
-echo "Pulling changes..."
-git pull --ff-only
-
-echo "Push commits..."
-git push
-
-echo "Revert uncommitted changes..."
-git checkout .
-
-echo "Cleaning..."
-mvn clean
 
 echo "Preparing release..."
 mvn release:prepare -DreleaseVersion=${VERSION} -DdevelopmentVersion=${NEXT_VERSION} -Dtag=v${VERSION} -Dresume=false -Darguments=-Dgpg.passphrase=${PASSPHRASE}
